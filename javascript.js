afc9e6ca1af16b27747d5146ac51a80c346dfcd5
Bruno Rekssua Pelentir <99697423+BrunoPelentir@users.noreply.github.com>
2022-06-09 22:13:14 -0300

    Criando Calculadora

---
+++ javascript.js
@@ -0,0 +1,28 @@
+function mais() {
+    let ideUm = document.getElementById('ideUm').value;
+    let ideDois = document.getElementById('ideDois').value;
+    let resultado = parseInt(ideUm) + parseInt(ideDois);
+    document.getElementById("ideResultado").value=resultado
+     
+}
+function menos() {
+    let ideUm = document.getElementById('ideUm').value;
+    let ideDois = document.getElementById('ideDois').value;
+    
+}
+function multiplicacao() {
+    let ideUm = document.getElementById('ideUm').value;
+    let ideDois = document.getElementById('ideDois').value;
+    
+}
+function divisao() {
+    let ideUm = document.getElementById('ideUm').value;
+    let ideDois = document.getElementById('ideDois').value;
+     
+}
+function soma() {
+    let ideUm = document.getElementById('ideUm').value;
+    let ideDois = document.getElementById('ideDois').value;
+   
+}
+
