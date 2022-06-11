5ad9d4dc5497438a38a4cf4e51da502276d8536a
Bruno Rekssua Pelentir <99697423+BrunoPelentir@users.noreply.github.com>
2022-06-10 22:10:04 -0300

    Criando Calculadora

--- javascript.js
+++ javascript.js
@@ -3,26 +3,72 @@ function mais() {
     let ideDois = document.getElementById('ideDois').value;
     let resultado = parseInt(ideUm) + parseInt(ideDois);
     document.getElementById("ideResultado").value=resultado
-     
+    console.log(ideUm+"+"+ideDois+"="+resultado);
+    if (ideUm && ideDois) {
+        let resultado = parseInt(ideUm) + parseInt(ideDois);
+    document.getElementById("ideResultado").value=resultado
+    
+    } else {
+        alert("Coloque mais um número")
+    }     
 }
 function menos() {
     let ideUm = document.getElementById('ideUm').value;
     let ideDois = document.getElementById('ideDois').value;
+    let resultado = parseInt(ideUm) - parseInt(ideDois);
+    document.getElementById("ideResultado").value=resultado
+    console.log(ideUm+"+"+ideDois+"="+resultado);
+    if (ideUm && ideDois) {
+        let resultado = parseInt(ideUm) - parseInt(ideDois);
+    document.getElementById("ideResultado").value=resultado
+    
+    } else {
+        alert("Coloque mais um número")
+    }
+    
+    
     
 }
 function multiplicacao() {
     let ideUm = document.getElementById('ideUm').value;
     let ideDois = document.getElementById('ideDois').value;
+    let resultado = parseInt(ideUm) * parseInt(ideDois);
+    document.getElementById("ideResultado").value=resultado
+    console.log(ideUm+"+"+ideDois+"="+resultado);
+    if (ideUm && ideDois) {
+        let resultado = parseInt(ideUm) * parseInt(ideDois);
+    document.getElementById("ideResultado").value=resultado
     
+    } else {
+        alert("Coloque mais um número")
+    }
 }
 function divisao() {
     let ideUm = document.getElementById('ideUm').value;
     let ideDois = document.getElementById('ideDois').value;
-     
+    let resultado = parseInt(ideUm) / parseInt(ideDois);
+    document.getElementById("ideResultado").value=resultado
+    console.log(ideUm+"+"+ideDois+"="+resultado);
+    if (ideUm && ideDois) {
+        let resultado = parseInt(ideUm) / parseInt(ideDois);
+    document.getElementById("ideResultado").value=resultado
+    
+    } else {
+        alert("Coloque mais um número")
+    }
 }
 function soma() {
     let ideUm = document.getElementById('ideUm').value;
     let ideDois = document.getElementById('ideDois').value;
-   
+    let resultado = parseInt(ideUm) = parseInt(ideDois);
+    document.getElementById("ideResultado").value=resultado
+    console.log(ideUm+"+"+ideDois+"="+resultado);
+    if (ideUm && ideDois) {
+        let resultado = parseInt(ideUm) - parseInt(ideDois);
+    document.getElementById("ideResultado").value=resultado
+    
+    } else {
+        alert("Coloque mais um número")
+    }
 }
 
